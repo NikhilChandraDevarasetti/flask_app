c1e03675530a92ffd6dac40ca564a6bd8225a50c
NikhilChandraDevarasetti <nikhildevarasetti@gmail.com>
2022-05-04 00:03:47 +0530

    final commit of flask app

--- app.py
+++ app.py
@@ -1,24 +1,271 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 from elasticsearch import Elasticsearch
+from flask_ngrok import run_with_ngrok
+import json
 
 app = Flask(__name__)
+run_with_ngrok(app)
+
 es_client = Elasticsearch("http://elastic:zWyJXAMxgPIuVXBBYxpC@localhost:9200")
-# es_client = Elasticsearch()
 
-# body = {"size": 100, "query": {"match_all": {"name": 'q'}}}
 
-@app.route('/', methods=["GET", "POST"])
-def index():
+@app.route('/' , methods=[ "GET" , "POST" ])
+def index() :
+    q = request.args.get("q")  # for GET method
+    g = request.args.get("g")
+    l = request.args.get("l")
 
-	#q = request.args.get("q") #for GET method
-	q = request.form.get("q") # for POST method
+    # q = request.form.get("q")  # for POST method
 
-	if q is not None:
+    one_or_more = [
+                        {"multi_match" : {"query" : q , "fields" : "director_name", "fuzziness": 2}},
+                        {"multi_match" : {"query" : g , "fields" : "genres" , "fuzziness" : 2}} ,
+                        {"multi_match" : {"query" : l , "fields" : "language" , "fuzziness" : 2}}
+                    ]
 
-		res = es_client.search(index='movies', body= {"size": 2, "query": {"match": {"director_name": q}}}) # {"query": {"prefix": {"name": q}}}
-		return render_template('index.html', q=q, response=res)
+    if q is not None and g is None and l is None :
 
-	return render_template('index.html')
+        body = {
+            "_source" : ["director_name", "movie_title", "genres", "language"],
+            "size" : 10 ,
+            "query" : {
+                "bool" : {
+                    "must" : [one_or_more[0]],
+                    "filter" : [
+                        {"range" : {"imdb_score" : {"gte" : "5.0"}}}
+                    ] ,
+                    "should" : [
+                        {"match" : {"language" : "hindi"}} ,
+                        # {"match" : {"genres" : "comedy"}},
+                        {"match" : {"language" : "english"}}
+                    ] ,
+                    "must_not" : [ ]
+                }
+            } ,
+            "suggest" : {
+                "my-suggest-1" : {
+                    "text" : q ,
+                    "term" : {
+                        "field" : "director_name"}
+                }
+            }
+        }
 
-if __name__ == "__main__":
-	app.run(debug=True)
+        res = es_client.search(index='movies' , body=body)
+
+        hits = res['hits']['hits']
+        resp = [each['_source'] for each in hits]
+        return json.dumps(resp)
+
+    elif q is None and g is not None and l is None:
+        body = {
+            "_source" : ["director_name", "genres", "language", "movie_title"],
+            "size" : 10 ,
+            "query" : {
+                "bool" : {
+                    "must" : [one_or_more[1]],
+                    "filter" : [
+                        {"range" : {"imdb_score" : {"gte" : "5.0"}}}
+                    ] ,
+                    "should" : [
+                        {"match" : {"language" : "hindi"}} ,
+                        # {"match" : {"genres" : "comedy"}},
+                        {"match" : {"language" : "english"}}
+                    ] ,
+                    "must_not" : [ ]
+                }
+            } ,
+            "suggest" : {
+                "my-suggest-2" : {
+                    "text" : g ,
+                    "term" : {
+                        "field" : "genres"}
+                }
+            }
+        }
+
+        res = es_client.search(index='movies' , body=body)
+
+        hits = res[ 'hits' ][ 'hits' ]
+        resp = [ each[ '_source' ] for each in hits ]
+        return json.dumps(resp)
+
+    elif q is None and g is None and l is not None :
+
+        body = {
+            "_source" : [ "director_name" , "genres" , "language" , "movie_title" ] ,
+            "size" : 10 ,
+            "query" : {
+                "bool" : {
+                    "must" : [ one_or_more[2] ] ,
+                    "filter" : [
+                        {"range" : {"imdb_score" : {"gte" : "5.0"}}}
+                    ] ,
+                    "should" : [
+                        {"match" : {"language" : "hindi"}} ,
+                        # {"match" : {"genres" : "comedy"}},
+                        {"match" : {"language" : "english"}}
+                    ] ,
+                    "must_not" : [ ]
+                }
+            }
+        }
+
+        res = es_client.search(index='movies' , body=body)
+
+        hits = res[ 'hits' ][ 'hits' ]
+        resp = [ each[ '_source' ] for each in hits ]
+        return json.dumps(resp)
+
+    elif q is not None and g is not None and l is None :
+
+        body = {
+            "_source" : [ "director_name" , "genres" , "language" , "movie_title" ] ,
+            "size" : 10 ,
+            "query" : {
+                "bool" : {
+                    "must" : one_or_more[0:2] ,
+                    "filter" : [
+                        {"range" : {"imdb_score" : {"gte" : "5.0"}}}
+                    ] ,
+                    "should" : [
+                        {"match" : {"language" : "hindi"}} ,
+                        # {"match" : {"genres" : "comedy"}},
+                        {"match" : {"language" : "english"}}
+                    ] ,
+                    "must_not" : [ ]
+                }
+            } ,
+            "suggest" : {
+                "my-suggest-1" : {
+                    "text" : q ,
+                    "term" : {
+                        "field" : "director_name"}
+                } ,
+                "my-suggest-2" : {
+                    "text" : g ,
+                    "term" : {
+                        "field" : "genres"}
+                }
+            }
+        }
+
+        res = es_client.search(index='movies' , body=body)
+
+        hits = res[ 'hits' ][ 'hits' ]
+        resp = [ each[ '_source' ] for each in hits ]
+        return json.dumps(resp)
+
+    elif q is None and g is not None and l is not None :
+
+        body = {
+            "_source" : [ "director_name" , "genres" , "language" , "movie_title" ] ,
+            "size" : 10 ,
+            "query" : {
+                "bool" : {
+                    "must" : one_or_more[1:],
+                    "filter" : [
+                        {"range" : {"imdb_score" : {"gte" : "5.0"}}}
+                    ] ,
+                    "should" : [
+                        {"match" : {"language" : "hindi"}} ,
+                        # {"match" : {"genres" : "comedy"}},
+                        {"match" : {"language" : "english"}}
+                    ] ,
+                    "must_not" : [ ]
+                }
+            } ,
+            "suggest" : {
+                "my-suggest-2" : {
+                    "text" : g ,
+                    "term" : {
+                        "field" : "genres"}
+                }
+            }
+        }
+
+        res = es_client.search(index='movies' , body=body)
+
+        hits = res[ 'hits' ][ 'hits' ]
+        resp = [ each[ '_source' ] for each in hits ]
+        return json.dumps(resp)
+
+    elif q is not None and g is None and l is not None :
+
+        body = {
+            "_source" : [ "director_name" , "genres" , "language" , "movie_title" ] ,
+            "size" : 10 ,
+            "query" : {
+                "bool" : {
+                    "must" : [ one_or_more[0], one_or_more[2] ] ,
+                    "filter" : [
+                        {"range" : {"imdb_score" : {"gte" : "5.0"}}}
+                    ] ,
+                    "should" : [
+                        {"match" : {"language" : "hindi"}} ,
+                        # {"match" : {"genres" : "comedy"}},
+                        {"match" : {"language" : "english"}}
+                    ] ,
+                    "must_not" : [ ]
+                }
+            } ,
+            "suggest" : {
+                "my-suggest-1" : {
+                    "text" : q ,
+                    "term" : {
+                        "field" : "director_name"}
+                }
+            }
+        }
+
+        res = es_client.search(index='movies' , body=body)
+
+        hits = res[ 'hits' ][ 'hits' ]
+        resp = [ each[ '_source' ] for each in hits ]
+        return json.dumps(resp)
+
+    elif q is not None and g is not None and l is not None :
+
+        body = {
+            "_source" : [ "director_name" , "genres" , "language" , "movie_title" ] ,
+            "size" : 10 ,
+            "query" : {
+                "bool" : {
+                    "must" : one_or_more,
+                    "filter" : [
+                        {"range" : {"imdb_score" : {"gte" : "5.0"}}}
+                    ] ,
+                    "should" : [
+                        {"match" : {"language" : "hindi"}} ,
+                        # {"match" : {"genres" : "comedy"}},
+                        {"match" : {"language" : "english"}}
+                    ] ,
+                    "must_not" : [ ]
+                }
+            } ,
+            "suggest" : {
+                "my-suggest-1" : {
+                    "text" : q ,
+                    "term" : {
+                        "field" : "director_name"}
+                } ,
+                "my-suggest-2" : {
+                    "text" : g ,
+                    "term" : {
+                        "field" : "genres"}
+                }
+            }
+        }
+
+        res = es_client.search(index='movies' , body=body)
+
+        hits = res[ 'hits' ][ 'hits' ]
+        resp = [ each[ '_source' ] for each in hits ]
+        return json.dumps(resp)
+
+    else: # return render_template('index.html')
+        return "enter query"
+
+
+if __name__ == "__main__" :
+    app.run(debug=True)
---
+++ app.wsgi
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, "C:/elastic_stack/flask_app")
+
+# activate_this = 'C:/elastic_stack/flask_app/venv/Scripts/activate_this.py'
+# with open(activate_this) as file_:
+#     exec(file_.read(), dict(__file__=activate_this))
+
+from app import app as application
